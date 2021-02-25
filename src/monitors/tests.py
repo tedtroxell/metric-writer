42e80f25d4e95706f27fdeee1c0227806776aed1
tedtroxell <ted@tedtroxell.com>
2021-02-25 15:03:06 -0500

    cleanup

--- src/monitors/tests.py
+++
@@ -1,7 +0,0 @@
-import unittest,pytest
-from unittest import TestCase
-
-class OODMonitorTest(TestCase):
-    @pytest.fixture(autouse=True)
-    def test_no_ood(self):
-        self.assertTrue(True,'----')
--- src/signals/interface.py
+++
@@ -1,7 +0,0 @@
-import inspect
-from typing import Union
-from src.interface import BaseInterface
-class BaseSignal(BaseInterface):
-
-    pass
-
